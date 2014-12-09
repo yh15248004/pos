73920f599095b98e066f8ab2547d683a340d8faa
yh <15248004@qq.com>
2014-12-09 23:01:53 +0800

    add cartItemDao & cartItemsDao

--- pos_v2_3rdlib/src/main.js
+++ pos_v2_3rdlib/src/main.js
@@ -1,6 +1,9 @@
 function printInventory(tags) {
-  var itemsInfos = new CartItemDao(tags);
+  var itemsInfos = new InputsDao(tags);
   //var inventroyText = getInventroyText(cartItems);
   var cartItems = itemsInfos.getCartItems();
-  console.log(cartItems);
+  //console.log(cartItems);
+  var inventoryInfos = new Inventory(cartItems);
+  var inventoryText = inventoryInfos.getInventoryText();
+  console.log(inventoryText);
 }
