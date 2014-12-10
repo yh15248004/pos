49bf7ddb828562bc0b7c12b81114bf02fafdbe1e
yh <15248004@qq.com>
2014-12-10 13:51:11 +0800

    clear the comments sentence from posv2

--- pos_v2_3rdlib/src/main.js
+++ pos_v2_3rdlib/src/main.js
@@ -1,8 +1,6 @@
 function printInventory(tags) {
   var itemsInfos = new InputsDao(tags);
-  //var inventroyText = getInventroyText(cartItems);
   var cartItems = itemsInfos.getCartItems();
-  //console.log(cartItems);
   var inventoryInfos = new Inventory(cartItems);
   var inventoryText = inventoryInfos.getInventoryText();
   console.log(inventoryText);
